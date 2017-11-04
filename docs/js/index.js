50e4c7d8caf41ef834875d8e3e9a23ee383463d6
9uilherme <9uilhermealencar@gmail.com>
2017-11-04 08:59:24 -0200

    melhorias

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,29 +1,27 @@
-function showHideByClass(element) {
+var app = angular.module('myApp', []);
+
+app.filter('greet', function() {
+    return function(name, namess, surname) {
+       return 'Hello, ' + name + '!';
+     };
+   })
+
+app.controller('indexController',  ['$scope', function($scope){
+
+    $scope.showNaoObrigatorios = false;
     
-    var list= document.getElementsByClassName(element);
-    [].forEach.call(list, function(el) {
-        if(el.style.display == 'none'){
-            el.style.display  = 'block';
-        }else{
-            el.style.display  = 'none';
-        }
-    });
-}
-
-function showHideById(element) {
-    var element = document.getElementById(element), i;
     
-    if(element.style.display == 'none'){
-        element.style.display  = 'block';
-    }else{
-        element.style.display  = 'none';
-    }
+    $scope.fontes = [
+        {id: 1, nome: "Cartório"},
+        {id: 2, nome: "Prestador de assistência à saúde" },
+        {id: 3, nome: "Parente" },
+        {id: 4, nome: "Outro" },
+        {id: 5, nome: "Desconhecido " }];
+        
+        $scope.paciente = {obito : {fonte: {id: 1, nome: "Cartório"}}};
+        
+    $scope.setShowNaoObrigatorios = function(value){
+        $scope.showNaoObrigatorios = value;
+    };
 
-}
-
-function showHideByClasses(arrayElement) {
-
-    arrayElement.forEach(function(element) {
-        showHideByClass(element);
-    });
-}
+}]);
